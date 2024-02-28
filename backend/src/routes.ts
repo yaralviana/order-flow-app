7af66627be8f5607819e7e2a3f35217806299fb8
Yara <yaralviana@gmail.com>
2024-02-28 19:54:26 -0300

    criando rota de detalhes de usuário

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
 
 const router = Router()
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/me', new DetailUserController().handle)
 
 export { router }
