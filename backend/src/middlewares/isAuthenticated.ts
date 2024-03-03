01e78b712d20ebc00099e6eff85cb5c0b65f7ff9
Yara <yaralviana@gmail.com>
2024-03-03 16:06:17 -0300

    configurando tipagem do express

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -20,6 +20,8 @@ export function isAuthenticated(req: Request, res: Response, next: NextFunction)
             process.env.JWT_SECRET
         ) as PayLoad
 
+        req.user_id = sub
+
         return next()
 
     } catch (err) {
