7ab4dfed7b44493f7ebd8c031ab7176b366f1dca
Yara <yaralviana@gmail.com>
2024-03-03 16:14:14 -0300

    retornando dados do usuário na requisição

--- backend/src/controllers/user/DetailUserController.ts
+++ backend/src/controllers/user/DetailUserController.ts
@@ -3,8 +3,14 @@ import { DetailUserService } from '../../services/user/DetailUserService'
 
 class DetailUserController {
     async handle(req: Request, res: Response) {
+
+        const user_id = req.user_id
+
+        console.log("id do user", user_id)
+
         const detailUserService = new DetailUserService()
-        const user = await detailUserService.execute()
+
+        const user = await detailUserService.execute(user_id)
 
         return res.json(user)
     }
