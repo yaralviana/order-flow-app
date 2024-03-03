abd2affd182e8c0182b9ae1265fb908bb6e79a5e
Yara <yaralviana@gmail.com>
2024-03-03 16:37:20 -0300

    criando rota de categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,11 +3,15 @@ import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 
 const router = Router()
-
+// -- ROTAS USER 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// -- ROTAS USER 
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export { router }
