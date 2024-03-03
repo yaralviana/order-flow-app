ff958617b86a60800b87841c33f842f4b7281479
Yara <yaralviana@gmail.com>
2024-03-03 16:47:47 -0300

    lista de categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
 
 const router = Router()
 // -- ROTAS USER 
@@ -13,5 +14,6 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 // -- ROTAS USER 
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router }
