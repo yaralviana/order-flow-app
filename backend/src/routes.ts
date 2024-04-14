21d7a256d7282ee314c5e1c83d09fe96027702e0
Yara <yaralviana@gmail.com>
2024-04-14 14:41:01 -0300

    Adição de itens ao pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -10,6 +10,8 @@ import { CreateProductController } from './controllers/product/CreateProductCont
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 import { CreateOrderController } from './controllers/order/CreateOrderController'
 import { RemoveOrderController } from './controllers/order/RemoveOrderController'
+import { AddItemController } from './controllers/order/AddITemController'
+
 
 
 import uploadConfig from './config/multer'
@@ -34,5 +36,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // -- ROTAS PEDIDOS
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
 
 export { router }
