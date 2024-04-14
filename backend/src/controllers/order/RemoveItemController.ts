1ed7fec2f9ee9fccf8f3f0d46bb3636ea7bcde55
Yara <yaralviana@gmail.com>
2024-04-14 14:58:38 -0300

    Correção de controller que remove itens dos pedidos

--- backend/src/controllers/order/RemoveItemController.ts
+++ backend/src/controllers/order/RemoveItemController.ts
@@ -3,11 +3,11 @@ import { RemoveItemService } from "../../services/order/RemoveItemService"
 
 class RemoveItemController {
     async handle(req: Request, res: Response) {
-        const item_id = req.query.order_id as string
+        const item_id = req.query.item_id as string
 
-        const removeOrder = new RemoveItemService()
+        const removeItemService = new RemoveItemService()
 
-        const order = await removeOrder.execute({
+        const order = await removeItemService.execute({
             item_id
         })
 
