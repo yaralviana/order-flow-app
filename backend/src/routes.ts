319463e03687cc1cc5ec224666b14038feffec79
Yara <yaralviana@gmail.com>
2024-04-14 16:07:10 -0300

    buscando detalhes de pedidos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -14,6 +14,7 @@ import { AddItemController } from './controllers/order/AddITemController'
 import { RemoveItemController } from './controllers/order/RemoveItemController'
 import { SendOrderController } from './controllers/order/SendOrderController'
 import { ListOrdersController } from './controllers/order/ListOrdersController'
+import { DetailOrderController } from './controllers/order/DetailOrderController'
 
 import uploadConfig from './config/multer'
 
@@ -41,5 +42,6 @@ router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 router.get('/orders', isAuthenticated, new ListOrdersController().handle)
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
 
 export { router }
