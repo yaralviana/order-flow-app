69d2ae01e9f2be2a16795abb77be43b0e4aac88b
Yara <yaralviana@gmail.com>
2024-04-28 12:16:47 -0300

    configurando estilo global do projeto

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <h1>
-      Order flow app
-    </h1>
+    <div className={styles.container}>
+      <h1>
+        Order flow app
+      </h1>
+    </div>
   );
 }
