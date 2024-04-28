f828a045fb2602d4716100d96564d1a4efb42b04
Yara <yaralviana@gmail.com>
2024-04-28 13:33:08 -0300

    iniciando tela de login e adicionando logo provisória

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,24 @@
-import styles from '../styles/Home.module.scss'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/home.module.scss'
+import logoImg from '../../public/logo.svg'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>
-        Order flow app
-      </h1>
-    </div>
-  );
+    <>
+      <Head>
+        <title>OrderFlowApp - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt='Logo Order flow app' color='#FFF' />
+        <div className={styles.login}>
+          <form>
+            <input type='text' />
+          </form>
+        </div>
+
+
+      </div>
+    </ >
+  )
 }
